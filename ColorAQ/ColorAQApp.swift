1cfac889a57cc48dc94031f43fb3d81c66ea7f43
Jennifer <designalkheymy.com>
2025-06-03 14:29:32 -0400

    Initial Commit

---
+++ ColorAQ/ColorAQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAQApp.swift
+//  ColorAQ
+//
+//  Created by Jennifer on 2025-06-03.
+//
+
+import SwiftUI
+
+@main
+struct ColorAQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
